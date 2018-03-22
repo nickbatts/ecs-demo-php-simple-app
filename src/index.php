fe2d50b70ac1daf0ab4c04e2746f701d9a2c8716
Nick Batts <nickbatts@users.noreply.github.com>
2018-03-22 12:25:41 -0600

    updated index.php

--- src/index.php
+++ src/index.php
@@ -20,9 +20,10 @@
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                        '''$myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
+                        fclose($myfile);'''
+                        echo "Nick Batts"
                 ?>
 
             </div>
